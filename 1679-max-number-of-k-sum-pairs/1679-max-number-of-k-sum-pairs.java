e7fd4ccdb5db53af7c257e878eb4b9af7c33289c
Laxmikant <laxmikantdadagi@gmail.com>
2026-07-13 00:01:24 +0530

    Time: 39 ms (19.07%), Space: 68.5 MB (87.65%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        int count=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(k-nums[i]) && map.get(k-nums[i])>0){
+                count++;
+                map.put(k-nums[i], map.get(k-nums[i])-1);
+            }else{
+                map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+            }
+        }
+        return count;
+    }
+}
